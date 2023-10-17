39abdb5274d1366269506234661251198c95acf6
Farbice <109209812+Farbice@users.noreply.github.com>
2023-10-18 00:00:13 +0200

    feat(App converter): handleChange and onInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useReducer, useRef, useState, useEffect } from 'react';
 import Intro from './Intro.jsx';
 import Count from './Count.jsx';
 
@@ -9,21 +9,91 @@ function App() {
     console.log(new FormData(e.target));
   }
 
-  const [value, setValue] = useState("")
-  const handleChange = (e) => {
-    setValue(e.target.value)
+  const [currencyValues, setCurrencyValues] = useState({
+    euroAmount: '',
+    dollarAmount: ''
+  });
+
+
+
+  const [dollarValue, setDollarValue] = useState('');
+  const [euroValue, setEuroValue] = useState('');
+
+  const dollarRef = useRef('');
+  //console.log(dollarRef.current);
+
+  const euroRef = useRef('');
+  //console.log(euroRef.current);
+
+  // useEffect(()=> {
+  //   setEuroValue(updatedValue *  0.95);
+  //   setDollarValue(euroValue * 1.05)
+
+  // }, [dollarValue, euroValue])
+
+  const handleChange = (e, currency) => {
+    //console.log(currency, e.target);
+    // console.log(e.target.value);
+    let updatedValue = 0;
+
+    switch (currency) {
+      case 'dollars':
+        updatedValue = parseFloat(e.target.value * 1.05).toFixed(2);
+        setTimeout(() => {
+          setEuroValue(updatedValue);
+          
+        }, 500);
+        break;
+      case 'euros':
+        updatedValue = parseFloat(e.target.value * 0.95).toFixed(2);
+        setTimeout(() => {
+          setDollarValue(updatedValue);
+          
+        }, 500);
+        break;
+      default:
+        break;
+    }
   }
 
-  console.log('render');
+  const handleInput = (e, refName) => {
+    console.log(refName);
+    
+    let updatedInputValue;
+
+    switch (refName) {
+      case 'dollarRef':
+        
+        console.log('here in dollarRef');
+        // console.log('dolls: ', updatedValue);
+        break;
+      case 'euroRef':
+
+        console.log('here in euroRef');
+        // console.log('euros: ', updatedValue);
+        break;
+      default:
+        break;
+    }
+
+  }
+
+
+  //console.log('render');
 
   return <>
     {/* <Intro /> */}
     {/* <Count /> */}
     <form onSubmit={handleSubmit}>
       <h3>Entrez le montant à convertir</h3>
-      <input type='number' name='amount' value={value} onChange={handleChange}/> €
-      <button>Convertir en US Dollar</button>
-      {value * 1.05}
+      <input type='number' name='euroAmount' defaultValue={dollarValue} ref={euroRef} onInput={(e) => { handleChange(e, 'dollars'); handleInput(e, 'dollarRef')} } /> €
+      <br></br>
+      {dollarValue}
+      <br></br>
+      <input type='number' name='dollarAmount' defaultValue={euroValue} ref={dollarRef} onInput={(e) => { handleChange(e, 'euros'); handleInput(e, 'euroRef')} } /> $
+      <br></br>
+      {euroValue}
+      <br></br>
     </form>
   </>
 }
