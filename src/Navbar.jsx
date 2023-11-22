9cf1433fb4816854848da9579e94bfdbfba2ec96
Farbice <109209812+Farbice@users.noreply.github.com>
2023-11-22 23:57:43 +0100

    feat(Converter component): font add and page styling

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -39,22 +39,27 @@ function Navbar() {
             window.removeEventListener('resize', handleScreenSize);
         }
 
-    }, [screenSize.width])
+    }, [screenSize.width]);
 
 
     return (
         <nav className="navbar flex md:h-24 w-full">
             <div className="flex md:w-3/4 md:m-auto items-center w-full mx-10 my-8">
                 <div className="logo w-1/2">
-                    <Logotype />
+                    <a href="">
+                        <Logotype />
+                    </a>
                 </div>
                 <div className="w-1/2 flex justify-end">
-                    <svg className={"burger-line" + ` ${showBurgerClass}`} viewBox='0 0 10 8' width='35'>
-                        <path d='M1 1h8M1 4h 8M1 7h8'
-                            stroke='#fff'
-                            strokeWidth={1}
-                            strokeLinecap='round' />
-                    </svg>
+                    <a href="#">
+                        <svg className={"burger-line" + ` ${showBurgerClass}`} height='30' width='28'>
+                            <g fill="none">
+                                <path stroke="#fff" d='M4 8 l20 0' strokeWidth={2} strokeLinecap='round' />
+                                <path stroke="#fff" d='M4 15 l20 0' strokeWidth={2} strokeLinecap='round' />
+                                <path stroke="#fff" d='M4 22 l20 0' strokeWidth={2} strokeLinecap='round' />
+                            </g>
+                        </svg>
+                    </a>
                     <ul className={"nav-list" + ` ${showNavItems}`}>
                         <li><a href="#">Favorites</a></li>
                         <li className="ml-8"><a href="#">Connect</a></li>
