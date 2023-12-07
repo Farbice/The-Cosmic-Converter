f1c52d59adc8027e45710416b018d5b0ae9dfbe6
Farbice <109209812+Farbice@users.noreply.github.com>
2023-12-07 23:58:54 +0100

    feat(Navbar): background change value test

--- src/Utilities/themes.js
+++ src/Utilities/themes.js
@@ -8,6 +8,7 @@ const themesManager = (theme) => {
             bckgd: 'bg-gradient-to-br from-blue-100 via-blue-300 to-blue-500',
             nav_bg: 'bg-slate-200',
             nav_text: 'text-slate-800',
+            test: 'bg-slate-300',
             icon : {
                 hover: 'bg-slate-300',
                 active: 'bg-slate-400',
@@ -21,6 +22,7 @@ const themesManager = (theme) => {
             bckgd: 'bg-gradient-to-br from-blue-900 via-blue-900 to-gray-900',
             nav_bg: 'bg-black',
             nav_text: 'text-slate-200',
+            test: 'bg-sky-300',
             icon : {
                 hover: 'bg-gray-800',
                 active: 'bg-gray-700',
