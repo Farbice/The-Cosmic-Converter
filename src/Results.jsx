9a356b8df7faec50864eadd32152b653518d3597
Farbice <109209812+Farbice@users.noreply.github.com>
2023-12-31 23:55:38 +0100

    feat(Results component): layout styling

--- src/Results.jsx
+++ src/Results.jsx
@@ -36,24 +36,28 @@ function Results(props) {
 
     return (
         <>
-            <div className={`flex flex-wrap -mt-10 mb-10 md:gap-8 justify-center xs:w-3/4 md:w-5/6 lg:w-3/5 mx-auto sm:p-16 p-8 ${themeColors.component.bckgd} ${themeColors.text} shadow-xl shadow-blue-500/20 border-none rounded-3xl`}>
+            <div className={`flex flex-wrap -mt-10 mb-10 md:gap-8 justify-start xs:w-3/4 md:w-5/6 lg:w-3/5 mx-auto sm:p-16 p-8 ${themeColors.component.bckgd} ${themeColors.text} shadow-xl shadow-blue-500/20 border-none rounded-3xl`}>
                 <div className="block w-full">
                     <div className="flex justify-between items-center">
                         <div className="p-8 bg-slate-200 rounded-3xl">
 
                         </div>
                         <div>
-                            <h3 className={`text-4xl font-questrial p-8 ${themeColors.title.bckgd}`}>Your {numberOfConversions.number} Cosmic {numberOfConversions.grammar}</h3>
+                            <h3 className={`text-4xl font-questrial p-8 rounded-3xl ${themeColors.title.bckgd}`}>Your {numberOfConversions.number} Cosmic {numberOfConversions.grammar}</h3>
                         </div>
                     </div>
                 </div>
-                <div className="block p-8 bg-orange-100 rounded-xl">
-
-                    <ul className="flex gap-8">
-                        <li>{state.inputData.amount}</li>
-                        <li>|</li>
-                        <li>{state.inputData.currency}</li>
-                    </ul>
+                <div className="relative md:inset-14 inset-6 flex gap-8 items-center">
+                    <div className="inline-block p-8 bg-orange-100 rounded-xl">
+                        <ul className="flex gap-8 md:text-3xl text-2xl">
+                            <li>{state.inputData.amount}</li>
+                            <li>|</li>
+                            <li>{state.inputData.currency}</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <p className={`md:text-4xl text-3xl ${themeColors.accent_text}`}>converts to</p>
+                    </div>
                 </div>
                 <div className="w-full">
                     <div className="flex flex-wrap w-full p-8 rounded-3xl border-[1px] border-orange-200">
